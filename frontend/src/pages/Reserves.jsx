5956205985b79b7fe429dd3e6f28faccd28ba00c
CarlaOnate <carlaonategardella@gmail.com>
2022-10-14 23:05:13 -0500

    styles for desktop reserva done

--- frontend/src/pages/Reserves.jsx
+++ frontend/src/pages/Reserves.jsx
@@ -1,9 +1,9 @@
+import { Button } from "antd";
 import React from "react";
-import Button from "../components/Button";
-import '../Estilos/Reservas.css'
 import { NavLink } from "react-router-dom";
 import { useLocation } from "react-router-dom";
-
+import cubeImg from '../images/Cubito.png';
+import graphImgs from '../images/imagenSeccion2Reservas.png'
 
 const Reservas = () => {
 
@@ -15,39 +15,23 @@ const Reservas = () => {
     
     return (
         <div>
-
             <div className="Seccion1">
-
-                <section className="ContenedorInicial2">
-
-                    <div className="ContenedorInicio">
-                        Reserva espacios o equipo dentro del Hub CSF
-
-                        <section className="Subtitulo">
-                            Desarrolla tus habilidades al máximo
-                        </section>
-                        
-                        {/* <Button texto = "Registrate y reserva ya" clase = 'CodigoPeque'/> */}
-
-                        <button className="CodigoPeque">
-                            <NavLink to="/login" className={paths.home}>Registrate y reserva ya!!</NavLink> 
-                        </button>
-
-                    </div>
-
-                </section>
-
+              <section className="ContenedorInicial2">
+                <div className="ContenedorInicio">
+                  <h1>Reserva espacios o equipo dentro del Hub CSF</h1>
+                  <p>Desarrolla tus habilidades al máximo</p>
+                  <Button className="orangeFilledButton">
+                      <NavLink to="/login" className={paths.home}>Registrate y empieza a reservar</NavLink>
+                  </Button>
+                </div>
+              </section>
             </div>
-            
             <div className="Seccion2">
                 <div className="ContenidoSeccion2Reservas">
-                    
                     <div className="TextosReservas">
-                        <div className="TituloSeccion2">Puedes Reservar</div>
-                        <div className="SubtituloSeccion2">Somos una organización líder en Ciberseguridad</div>
-                        <div className="TextoSeccion2">Al ser parte del hub puedes reservar espacios o equipos físicos</div>
+                        <h2>Puedes Reservar</h2>
+                        <p>Al ser parte del hub puedes reservar espacios o equipos físicos</p>
                     </div>
-
                     <div className="ListasReservas">
                         <ul className="Listadesordenada">
                             <li>Equipos de computo</li>
@@ -57,35 +41,25 @@ const Reservas = () => {
                         </ul>
                     </div>
                 </div>
-
                 <div className="ImagenSeccion2Reservas">
-                    <img src='../Imagenes/Cubito.png' alt="Esta el la imagen del cubito"/>
+                    <img src={cubeImg} alt="Esta el la imagen del cubito"/>
                 </div>
-
             </div>
 
             <div className="Seccion3">
-                
                 <div className="ImageSeccion3">
-                    <img src="../Imagenes/imagenSeccion2Reservas.png" alt="Imagen seccion 3 Reservas"/>
+                    <img src={graphImgs} alt="Imagen seccion 3 Reservas"/>
                 </div>
-
                 <div className="TextoSeccion3">
-
-                    <div className="TituloSeccion2">Revisa tus reservas</div>
-
-                    <div className="Subtitulo2Seccion2">Puedes ver tus reservas y el historial de reservas</div>
-
+                    <h2>Revisa tus reservas</h2>
+                    <p>Puedes ver tus reservas y el historial de reservas</p>
                     <ul>
                         <li>Revisa tus reservas actuales</li>
                         <li>Encuentra la ubicación de tus reservas</li>
                         <li>Ponte en contacto con los administradores del HUB</li>
                     </ul>
-
                 </div>
-
             </div>
-
         </div>
     )
 }
